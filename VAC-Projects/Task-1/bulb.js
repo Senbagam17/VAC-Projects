deb60b8cede2a5916d7891d240465d6dee61ba7d
Senbagam17 <138811571+Senbagam17@users.noreply.github.com>
2023-07-31 16:52:44 +0530

    Add files via upload

---
+++ VAC-Projects/Task-1/bulb.js
@@ -0,0 +1,7 @@
+function on(){
+    document.getElementById('img').src='https://e7.pngegg.com/pngimages/33/977/png-clipart-incandescent-light-bulb-lighting-transparency-and-translucency-bulb-light-fixture-lamp.png'
+    
+}
+function off(){
+    document.getElementById('img').src='https://e7.pngegg.com/pngimages/45/624/png-clipart-incandescent-light-bulb-lighting-led-lamp-bulb-logo-light-fixture-lantern-thumbnail.png'
+}
---
+++ VAC-Projects/Task-3/index3.js
@@ -0,0 +1,11 @@
+
+import Formikex from "./Formikex";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Formikex />
+  </StrictMode>
+);
